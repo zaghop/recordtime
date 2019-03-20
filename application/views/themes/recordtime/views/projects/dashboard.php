1f129feb742fda05f016e1ea75f166fc6ff20cf7
Kevin Moore <k.james.moore@gmail.com>
2019-03-20 18:01:49 -0400

    /project/dashboard view and view logic improvements.

--- application/views/themes/recordtime/views/projects/dashboard.php
+++ application/views/themes/recordtime/views/projects/dashboard.php
@@ -18,50 +18,59 @@
 
       <?php foreach ($projects as $project): ?>
 
-        <?php if (isset($project['start_date'])): ?>
-            <div class="ProjectItem ">
-              <p class="ProjectItem__Started">Started
-                <?php echo $project['start_date']; ?>
-              <p>
-              <div class="ProjectItem__Details row">
-                <div class="col-sm-3">
-                  <img
-                    src="<?= site_url(); ?>assets/images/user-placeholder.jpg"
-                  />
-                </div>
-                <div class="col-sm-3">
-                  <h3><?php echo $project['name']; ?></h3>
 
-                  <p class="end_date">
-                    <?php echo "Ends " ?>
-                    <?php echo date($project['end_date']);?>
-                  </p>
-                  <p class="producer">
-                    <?php echo $project['firstname'];
-                          echo " ";
-                          echo $project['lastname'];
-                    ?>
-                  </p>
-                  <p class="cost">
-                    <?php
-                      echo $project['project_cost'];
-                    ?>
-                  </p>
-                </div>
-                <div class="col-sm-3">
-                  Basic Overview
-                  <p class="name"><?php echo $project['songs']; echo " songs"; ?></p>
-                  <p class="name"><?php echo $project['name']; ?></p>
-                </div>
-                <div class="col-sm--3">
-                  PDF
-                  Change
+        <div class="ProjectItem">
+
+          <?php if (isset($project['start_date'])): ?>
+                <p class="started">Started
+                  <?php echo $project['start_date']; ?>
+                <p>
+          <?php else: ?>
+              <div class="ProjectItem--overlay">
+                <div class="details">
+                  <h3><?php echo $project['name']; ?></h3>
+                  <button class="start">Start</button>
                 </div>
               </div>
-            </div>
-          <?php else: ?>
-          <div>waiting to start</div>
           <?php endif; ?>
+
+
+            <div class="ProjectItem__Details row">
+              <div class="col-sm-3">
+                <img
+                  src="<?= site_url(); ?>assets/images/user-placeholder.jpg"
+                />
+              </div>
+              <div class="col-sm-3">
+                <h3><?php echo $project['name']; ?></h3>
+
+                <p class="end_date">
+                  <?php echo "Ends " ?>
+                  <?php echo date($project['end_date']);?>
+                </p>
+                <p class="producer">
+                  <?php echo $project['firstname'];
+                        echo " ";
+                        echo $project['lastname'];
+                  ?>
+                </p>
+                <p class="cost">
+                  <?php
+                    echo $project['project_cost'];
+                  ?>
+                </p>
+              </div>
+              <div class="col-sm-3">
+                Basic Overview
+                <p class="name"><?php echo $project['songs']; echo " songs"; ?></p>
+                <p class="name"><?php echo $project['name']; ?></p>
+              </div>
+              <div class="col-sm--3">
+                PDF
+                Change
+              </div>
+            </div>
+          </div>
 
 
         <?php endforeach; ?>
