48ee825adb38c6a84c44a5230d3224ea27c5c4e6
Zaghop <jonathanljames+gh@gmail.com>
2019-05-29 03:31:04 +0000

    Messaging new file

---
+++ application/models/Message_model.php
@@ -0,0 +1,170 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Message_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->library('upload');
+    }
+	
+	
+	public function addmessage($data = array()) {
+        //add created and modified data if not included
+        if(!array_key_exists("sent", $data)){
+            $data['sent'] = date("Y-m-d H:i:s");
+        }
+		
+		if(!array_key_exists("attachments", $data)){
+            $data['attachments'] = '';
+        }
+        
+        
+        //insert user data to users table
+        $insert = $this->db->insert('tblmessages', $data);
+        
+        //return the status
+        if($insert){
+            $insertId = $this->db->insert_id();
+			$this->db->select('*');
+			$this->db->from('tblmessages');
+			$this->db->where('id', $insertId );
+			$query = $this->db->get();
+
+			if ( $query->num_rows() > 0 )
+			{
+				$row = $query->result_array();
+				return $row;
+			}
+			
+        }else{
+            return false;
+        }
+    }
+	
+	function getRows($params = array()){
+		//echo "test"; exit;
+			$a = $params['from_id'].$params['to_id'];
+			$b = $params['to_id'].$params['from_id'];
+			$this->db->select('*');
+			$this->db->from('tblmessages');
+			//$this->db->where('thread_id',$params['from_id'].$params['to_id']);
+			//$this->db->where('thread_id',$params['to_id'].$params['from_id']);
+			$where = '(thread_id='.$a.' or thread_id = '.$b.')';
+			$this->db->where($where);
+			$query = $this->db->get();
+			$result = $query->result_array();
+
+
+
+
+		//return fetched data
+		return $result;
+	}
+	
+	function getRecipientdetails($params = array()){
+		//echo "test"; exit;
+			$user_id = $params['id'];
+			$rec_id = $params['rec_id'];
+			
+			$this->db->select('to_id,from_id');
+			$this->db->distinct();
+			$this->db->from('tblmessages');
+			$this->db->where('from_id', $user_id );
+			
+			$query = $this->db->get();
+			if ( $query->num_rows() > 0 )
+			{
+				$row = $query->result_array();
+				//echo "<pre>"; print_r($row);
+				$row1 = array();
+				$row2 = array();
+				foreach($row as $rows) {
+					$userto_id = $rows['to_id'];
+					$userfrom_id = $rows['to_id'];
+					//
+					$this->db->select('id,firstname,lastname');
+					$this->db->from('tbl_user');
+					$this->db->where('id', $userto_id );
+					$query1 = $this->db->get();
+					$row1[] = $query1->result_array();
+					
+					
+					$this->db->select('message');
+					$this->db->from('tblmessages');
+					$this->db->limit(1);
+					$this->db->order_by('id',"DESC");
+					$where = '(to_id='.$userto_id.' OR from_id = '.$userto_id.')';
+					$this->db->where($where);
+					$query2 = $this->db->get();
+					$row2[] = $query2->result_array();
+					 
+					
+				}
+				return $row1;
+				
+//				if ( $query1->num_rows() > 0 )
+//				{
+//					$row1 = $query1->result_array();
+//					//echo "<pre>"; print_r($row1);
+//					foreach($row1 as $rows1) {
+//						return $rows1;
+//					}
+//
+//				}
+				
+				
+				
+				
+			}
+
+		//return fetched data
+		//return $result;
+	}
+	
+	function getRecipientmsg($params = array()){
+		//echo "test"; exit;
+			$user_id = $params['id'];
+			$rec_id = $params['rec_id'];
+			
+			$this->db->select('to_id,from_id');
+			$this->db->distinct();
+			$this->db->from('tblmessages');
+			$this->db->where('from_id', $user_id );
+			
+			$query = $this->db->get();
+			if ( $query->num_rows() > 0 )
+			{
+				$row = $query->result_array();
+				//echo "<pre>"; print_r($row);
+				
+				$row2 = array();
+				foreach($row as $rows) {
+					$userto_id = $rows['to_id'];
+					//$userfrom_id = $rows['from_id'];
+				
+					$this->db->select('message');
+					$this->db->from('tblmessages');
+					$this->db->limit(1);
+					$this->db->order_by('id',"DESC");
+					$where = '(to_id='.$userto_id.' OR from_id = '.$userto_id.')';
+					$this->db->where($where);
+					$query2 = $this->db->get();
+					$row2[] = $query2->result_array();
+					 
+					
+				}
+				return $row2;
+				
+			}
+
+		}
+	
+
+ 
+
+
+}
